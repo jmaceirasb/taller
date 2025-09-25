71dd7ee8312452bbaa393fe4863d1241c7874b23
javierDAW <jmaceirasb@hotmail.com>
2025-09-25 13:01:29 +0200

    Programa Interactivo

--- lib/Hola.php
+++ lib/Hola.php
@@ -1,6 +1,7 @@
 <?php
-//Autor : Javier M. <jmaceiras@ho..>
-//El nombre por defecto es Mundo
-$nombre = isset($argv[1]) ? $argv[1] : "Mundo";
+// Autor: Javier M <jmace@...>
+print "Introduce tu nombre:";
+$nombre = trim(fgets(STDIN));
 @print "Hola, {$nombre}\n";
+
 
